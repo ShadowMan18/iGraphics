f2c175ec93e70463bf958cf8a774cabbb7da3762
Shadman Sami Shanon <shadmansami3s@gmail.com>
2025-06-13 20:55:29 +0600

    Update iGraphics files

---
+++ iGraphics Demo Code/iMain.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#include <iGraphics.h>
+using namespace std;
+ 
+void iDraw()
+{
+	iClear();
+}
+
+void iMouse(int button, int state, int mx, int my)
+{
+}
+
+void iMouseMove(int mx, int my)
+{
+}
+
+void iPassiveMouseMove(int mx, int my)
+{
+}
+
+void iKeyboard(unsigned char k)
+{
+}
+
+void iSpecialKeyboard(unsigned char k)
+{
+}
+
+int main()
+{
+	iInitialize(1200, 600, "iGraphics");
+	return 0;
+}
